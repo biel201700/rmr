b624a4a460eda99c5fb97af955c612e7c07da06b
biel201700 <bje0214@biel.co.kr>
2021-05-31 18:17:39 +0900

    commit -m "막장mapping 양식 html"

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var pdfDocument = require('pdfkit');
 var fs = require('fs');
 var request = require('request');
-
-var gothic = require('../public/font/gothic.js');
+var {jsPDF} = require('jspdf');
 
 // 메인페이지
 router.get('/', function (req, res, next) {
@@ -23,69 +21,19 @@ router.get('/pdf', function (req, res, next) {
     res.render('pdf', {id: req.query.id});
 });
 
-// 메인페이지
+// pdf 페이지
 router.get('/pdf.do', function (req, res, next) {
+    var doc = new jsPDF('p', 'mm', 'a4')
 
-    // Create PDF
-    var doc = new pdfDocument();
+    var xml = new XMLHttpRequest();
+    const myFont = xml.open("GET", "/public/font/gothic.js", true);
+    doc.addFileToVFS("MyFont.ttf", myFont);
+    doc.addFont("MyFont.ttf", "MyFont", "normal");
+    doc.setFont("MyFont");
 
-    var data = [
-        {label: '국어', value: 76},
-        {label: '수학', value: 48},
-        {label: '과학', value: 89},
-        {label: '사회', value: 68},
-        {label: '음악', value: 55},
-        {label: '영어', value: 73},
-        {label: '기술', value: 92},
-        {label: '미술', value: 58},
-        {label: '선택', value: 79}
-    ];
-
-
-    var doc = new pdfDocument();
-    var page_w = doc.page.width;
-    var page_h = doc.page.height;
-
-
-    doc.registerFont('font', gothic);
-    doc.reg
-    doc.font('font');
-
-    doc.fontSize(30)
-        .text('성적 그래프', 20, 20);
-
-// 그래프 그리기
-    var margin = 20;
-    var g_w = page_w - margin * 2 - 50;
-    var g_x = margin + 50;
-    var y = 80;
-    var wpx = g_w / 100;
-    for (var i = 0; i < data.length; i++) {
-        var value = data[i].value;
-        var label = data[i].label;
-        doc.save()
-            .rect(g_x, y, wpx * value, 20)
-            .fill((i % 2) ? 'skyblue' : 'pink');
-        doc.fontSize(10)
-            .fillColor("black")
-            .text(label, 30, y + 5)
-            .text(value, g_x + 5, y + 5);
-        y += 20 + 5;
-    }
-
-    doc.end();
-
-    // Write headers
-    res.writeHead(200, {
-        'Content-Type': 'application/pdf',
-        'Access-Control-Allow-Origin': '*',
-        'Content-Disposition': 'attachment; filename=Untitled.pdf'
-    });
-
-    // Pipe generated PDF into response
-    doc.pipe(res);
-
-    doc.end();
+    doc.line(15, 19, 195, 19); // 선그리기(시작x, 시작y, 종료x, 종료y)
+    doc.text(15, 40, '안녕하세요'); // 글씨입력(시작x, 시작y, 내용)
+    doc.save('rmr.pdf');  //결과 출력
 });
 
 // 라우터를 모듈로 만든다
